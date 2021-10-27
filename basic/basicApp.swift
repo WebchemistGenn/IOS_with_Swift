f8c500f3f634ed89f0d9bb57c02daf2656d695a7
YoonWonYoul <yoonwonyoul@webchemist.net>
2021-10-28 00:56:52 +0900

    Initial Commit

---
+++ basic/basicApp.swift
@@ -0,0 +1,17 @@
+//
+//  basicApp.swift
+//  basic
+//
+//  Created by Webchemist on 2021/10/28.
+//
+
+import SwiftUI
+
+@main
+struct basicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
